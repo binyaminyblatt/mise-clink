1ba53b0171ef2aa915ed2947d8df8b302e5e5577
IMXEren <96839938+IMXEren@users.noreply.github.com>
2025-11-04 06:46:29 +0530

    fix: remove __MISE_CLINK prefixed env vars when `mise.ps1` is invoked

--- mise.ps1
+++ mise.ps1
@@ -2,4 +2,8 @@
 ## which is only compatible with cmd + clink. Hence, this 'mise.ps1' is here to override that behaviour.
 
 $env:Path = $env:Path.Replace("$PSScriptRoot;", "")
+Remove-Item -ErrorAction SilentlyContinue -Path Env:\__MISE_CLINK_ACTIVATED
+Remove-Item -ErrorAction SilentlyContinue -Path Env:\__MISE_CLINK_CMD_ACTIVATED
+Remove-Item -ErrorAction SilentlyContinue -Path Env:\__MISE_CLINK_HOOK_ENV_ARGS
+Remove-Item -ErrorAction SilentlyContinue -Path Env:\CLINK_PID
 & mise.exe $args
